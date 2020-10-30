bd3a9aa8e4dba7e41063592529cef9768cdba216
Eric Bazan <ebazan7029@gmail.com>
2020-10-30 03:43:52 -0400

    Re-Organized and did some restructuring for the html pages associated to the job search page

--- aviation-project/aviation/settings.py
+++ aviation-project/aviation/settings.py
@@ -143,9 +143,9 @@ WSGI_APPLICATION = 'aviation.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ajb_sprint3',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': '123miles', #Put the password that you used for PostrgeSQL
+        'PASSWORD': 'pgbazerpass', #Put the password that you used for PostrgeSQL
         'HOST': 'localhost',
         'PORT': '5432',
     }
